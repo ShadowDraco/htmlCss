f4fd59a9d253db0be796ea3c264fe7b8bff56184
EStorm2002 <stormethan10@gmail.com>
2017-01-18 07:32:00 -0800

    Add files via upload
    Javascript online

---
+++ code.js
@@ -0,0 +1,18 @@
+void setup() {
+size(800,800);
+}
+
+var xPos = 200;
+var yPos = 200;
+
+void draw() {
+
+xPos = mouseX;
+yPos = mouseY;
+background(25,150,100);
+fill(100,50,100);
+rect(200,200,200,200);
+fill(50,100,50);
+rect(xPos,yPos,50,50);
+
+};
